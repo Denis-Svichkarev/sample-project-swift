35bf98ce44101102bce735d97201cc68ac6f70e0
Denis Svichkarev <denis.svichkarev@gmail.com>
2024-06-29 11:56:47 +0200

    Setup initial project structure with folders for MVVM and resources (#1)

---
+++ sample-project/SampleProject/SampleProject/Views/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Denis Svichkarev on 29/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
